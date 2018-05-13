0bf5fe823a91245e33d491f7f60a9fa413415cf4
xiao <u6277077@anu.edu.au>
2018-05-13 12:57:48 +1000

    path

--- src/comp1110/ass2/gui/Game.java
+++ src/comp1110/ass2/gui/Game.java
@@ -42,7 +42,8 @@ public class Game extends Application {
     int i1,i2,i3,i4=0;
     public  int numPlayers;
     public static String posChars="456789YZ0123STUVWXMNOPQRGHIJKLABCDEF";
-    private static final AudioClip error = new AudioClip(Game.class.getResource("assets\\error.wav").toString());
+//    private static final String LOOP_URL=Game.class.getResource("assets/error.wav").toString();
+    private static final AudioClip error = new AudioClip(Game.class.getResource("assets/error.wav").toString());
     private Color []flagColor={Color.LIGHTYELLOW,Color.LIGHTBLUE, Color.PINK, Color.LIGHTGREEN, Color.LIGHTSALMON, Color.LAVENDERBLUSH, Color.LIGHTCORAL};
 
     //the menu bar
