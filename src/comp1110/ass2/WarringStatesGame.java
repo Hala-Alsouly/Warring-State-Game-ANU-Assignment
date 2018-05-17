e3240161200fa230705eecbdd021294d0af61c06
Cassie <u6416655@anu.edu.au>
2018-05-17 23:10:17 +1000

    Modified task10!

--- src/comp1110/ass2/WarringStatesGame.java
+++ src/comp1110/ass2/WarringStatesGame.java
@@ -592,13 +592,12 @@ public class WarringStatesGame {
         // FIXME Task 10: generate a legal move
 
         //add legal moves in an Arraylist, and randomly pick up location chars from it
-        int[] numbers = {65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
-                81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
-                48, 49, 50, 51, 52, 53, 54, 55, 56, 57};
+        char[] places = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R',
+                'S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9'};
         ArrayList<Character> legalMove = new ArrayList<>();
-        for (int num : numbers) {
-            //
-            char locationChar = (char) num;
+        for (char p: places) {
+            //add legal locationChar in the Arraylist 'legalMove'
+            char locationChar = p;
             if (isMoveLegal(placement, locationChar)) {
                 legalMove.add(locationChar);
             }
